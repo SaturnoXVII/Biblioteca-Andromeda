b41483cfcf1a0e686b47b0b2598c4c9a58b858f7
SaturnoXVII <pequenojupiter@gmail.com>
2026-04-13 11:46:12 -0300

    Ajustes

--- pages/index.php
+++ pages/index.php
@@ -1,10 +1,20 @@
 <?php 
 session_start();
-include "../config/conexao.php";
-include "../config/crud.php";
-
+include ("../config/conexao.php");
+include ("../config/crud.php");
 
 ?>
 
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="biblioteca-andromeda/assets/css/style.css">
+    
+    <title>Biblioteca Andromeda</title>
+</head>
+<body> 
 
 
---
+++ pages/login.php
@@ -0,0 +1,6 @@
+<?php  
+session_start();
+include "../config/conexao.php";
+
+// http://10.68.49.32/Biblioteca-Andromeda/
+?>
